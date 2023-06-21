1a3a5564ad18fbfe74b0457a5a6d33544e41eff3
Kirill Demidov <kademidov@gmail.com>
2023-06-21 10:27:12 +0300

    Изменения: - Использование jQuery для мобильного меню

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,98 +1,37 @@
-function sum(a, b) {
-  console.log('BEFORE');
-  return a + b;
-}
+$(document).ready(function() {
 
-let c = {
-  name: 'Kesha'
-}
-let x = 6;
+  // let isOpen = false;
 
-let sum1 = sum(c, x);
-let sum2 = sum(1, 3);
+  $('.js-burger').on('click', function() {
+    $('.js-menu').slideToggle();
 
-console.log(sum1);
-console.log(sum2);
+    // if (isOpen) {
+    //   $('.js-menu').slideUp();
+    //   isOpen = false;
+    // } else {
+    //   $('.js-menu').slideDown();
+    //   isOpen = true;
+    // }
+  });
 
-// let services = document.querySelectorAll('.services-item');
-
-// services.style.color = 'red';
-// services.style.border = '2px solid green';
-
-
-// for (let i = 0; i < services.length; i++) {
-//   services[i].classList.add('services-item-test');
-// }
-
-// services.forEach( function(item) {
-//   item.classList.add('services-item-test');
-// } );
-
-
-function runCustomFunction(a) {
-  a();
-}
-
-let iNeedSleep = function() {
-  console.log('Я обещаю два дня из 7 накануне занятий, в понедельник и среду, ложиться спать рано, что бы выспаться');
-}
-
-function iWantShoot() {
-  console.log('Пиф-паф');
-}
-
-runCustomFunction(iNeedSleep);
-runCustomFunction(iWantShoot);
-
-runCustomFunction( function() {
-  console.log('Я сообщение из безымянной функции, которая никуда не записана');
-} );
-
-
-let burger = document.querySelector('.js-burger');
-let menu = document.querySelector('.js-menu');
-
-burger.addEventListener('click', function() {
-  let isOpen = menu.classList.contains('menu-open');
-
-  if ( isOpen ) {
-    menu.classList.remove('menu-open');
-  } else {
-    menu.classList.add('menu-open');
-  }
 });
 
+// let burger = document.querySelector('.js-burger');
+// let menu = document.querySelector('.js-menu');
 
-// let menuLinks = document.querySelectorAll('.menu a');
+// burger.addEventListener('click', function() {
+//   let isOpen = menu.classList.contains('menu-open');
 
-// menuLinks.forEach(function(link) {
-//   link.addEventListener('click', function(event) {
-//     event.preventDefault();
-//   });
+//   if ( isOpen ) {
+//     menu.classList.remove('menu-open');
+//   } else {
+//     menu.classList.add('menu-open');
+//   }
 // });
 
 
 
-$(document).ready(function() {
 
 
-  console.log('jQuery working!');
 
 
-});
-
-
-// 1 Сздать массив
-// 2 перебрать его
-// 2.1 для каждого элемента массива провести проверку на длинну
-
-
-// ЗАДАНИЕ №2
-// 1. Найти на странице DOM-элемент логотипа в шапке
-// 2. Повесить на него слушатель события клика
-// 3. При клике выводить в консоль - "Ура! Меня жмакнули!"
-
-// ЗАДАНИЕ №3
-// 1. Найти все ссылки из меню
-// 2. По очереди какждой ссылке повесить обработчик клика
-// 3. При клике на какую-то из ссылок выводить в консоль ее индекс
