6766280bd24ed0f1bb78978ca0e38c3176655dad
Kirill Demidov <kademidov@gmail.com>
2023-06-24 16:22:08 +0300

    табы на странице контактов и фильтры в работах

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -14,6 +14,56 @@ $(document).ready(function() {
     // }
   });
 
+  // АККОРДИОНЫ НА СТРАНИЦЕ FAQ
+  let prevBtn;
+  $('.js-accordion-btn').on('click', function() {
+    if (prevBtn === this) {
+      $(this).next().slideToggle();
+    } else {
+      $(prevBtn).next().slideUp();
+      $(this).next().slideDown();
+      prevBtn = this;
+    }
+  });
+
+  // Табы на транице - контакты
+  $('.js-tab-link').on('click', function(event) {
+    event.preventDefault();
+    $('.js-tab-link').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.js-tab-link');
+
+    $('.js-contacts-item').removeClass('active');
+    $('.js-contacts-item').eq(index).addClass('active');
+  });
+
+  // Фильтрация в наших работах
+  $('.js-filter-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    let filterType = $(this).data('filter');
+
+    if (filterType === 'all') {
+      $('.js-works-item').show();
+      return;
+    }
+
+    $('.js-works-item').each(function () {
+      let dataType = $(this).data('type');
+
+      if (filterType === dataType) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+
+  });
+
 });
 
 // let burger = document.querySelector('.js-burger');
